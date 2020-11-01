bb2f3d31580b86b06cee6fa79029644d8230a843
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-11-01 11:18:53 +0530

    Add files via upload

---
+++ moustache.js
@@ -0,0 +1,12 @@
+function preload(){}
+
+function setup() {
+    canvas=createCanvas(400,300);
+    canvas.center();
+}
+
+function draw() {}
+
+function takeSnapshot() {
+    save('myMoustache.png');
+}
